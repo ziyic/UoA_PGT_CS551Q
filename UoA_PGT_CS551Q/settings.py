c9f9f10829db90b148d0d89cb992dd904995fc53
LoveZym <1027388409@qq.com>
2021-03-29 08:18:13 +0100

    test(Test): Fixed a problem encountered in test
    Fixed a problem encountered in test

--- UoA_PGT_CS551Q/settings.py
+++ UoA_PGT_CS551Q/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
 from pathlib import Path
+import sys
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -79,6 +80,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'TEST': {
+            'MIRROR': 'default',
+        }
     }
 }
 
--- fires/tests.py
+++ fires/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 import random
 
+
 # Create your tests here.
 
 
@@ -10,7 +11,7 @@ class MainTest(TestCase):
         Test the index page, whose title is Global Fire Emissions, used template fires/index.html.
         :return:
         """
-        res = self.client.get('/views.index')
+        res = self.client.get('/')
         self.assertEqual(res.status_code, 200)
         self.assertContains(res, "<title>Global Fire Emissions</title>")
         self.assertTemplateUsed(res, "fires/index.html")
@@ -21,11 +22,12 @@ class MainTest(TestCase):
         :return:
         """
 
-        page_num = random.randint(1, 7)
-        res = self.client.get("regions?page={}".format(page_num))
+        page_num = random.randint(1, 10)
+        res = self.client.get(f'/regions?page={page_num}')
         self.assertEqual(res.status_code, 200)
-        self.assertContains(res, page_num)
+        self.assertContains(res, "Page {}".format(page_num))
         self.assertTemplateUsed(res, "fires/regions.html")
+        # test passed
 
     def test_region_detail(self):
         """
@@ -37,10 +39,10 @@ class MainTest(TestCase):
         valid_id = random.randint(1, 5)
         invalid_id = random.randint(-5, -1)
         # invalid test
-        invalid_res = self.client.get("region/{}".format(invalid_id))
+        invalid_res = self.client.get("/region/{}".format(invalid_id))
         self.assertEqual(invalid_res.status_code, 404)
         # valid test
-        valid_res = self.client.get("region/{}".format(valid_id))
+        valid_res = self.client.get("/region/{}".format(valid_id))
         self.assertEqual(valid_res.status_code, 200)
         self.assertTemplateUsed(valid_res, "fires/region_details.html")
         self.assertContains(valid_res, valid_id)
@@ -52,26 +54,29 @@ class MainTest(TestCase):
         Also, replace code below to test your functions controlling database works correctly.
         :return:
         """
-        test_regions = ["Los Angeles", "San Fransisco", "Washington D.C"]
+        test_regions = ["China", "Australia", "United Kingdom of Great Britain and Northern Ireland"]
         random_region = random.choice(test_regions)
         random_year = random.randint(2010, 2020)
-        res = self.client.get("fire/{}/{}".format(random_region, random_year))
+        res = self.client.get('/fire/{}/{}'.format(random_region, random_year))
         self.assertEqual(res.status_code, 200)
         self.assertContains(res, random_year)
         self.assertContains(res, random_region)
         self.assertTemplateUsed(res, "fires/fire_year.html")
+        # test not passed
 
     def test_fire_detail_by_type(self):
         """
         Test fire detail by type page.
         :return:
         """
-        test_regions = ["Los Angeles", "San Fransisco", "Washington D.C"]
-        test_types = ["Big", "Large", "Horrible", "Small", "Middle"]
+        test_regions = ["China", "Australia", "United Kingdom of Great Britain and Northern Ireland"]
+        test_types = ["TCC_All_fires", "TCC_TEMF_Fires", "TCC_AGRI_fires",
+                      "Total_Area_Burned", "TCC_DEFO_fires", "TCC_SAVA_Fires"]
         random_region = random.choice(test_regions)
         random_type = random.choice(test_types)
-        res = self.client.get("fire/{}/{}".format(random_region, random_type))
+        res = self.client.get("/fire/{}/{}".format(random_region, random_type))
         self.assertContains(res, random_type)
         self.assertContains(res, random_region)
         self.assertEqual(res.status_code, 200)
-        self.assertTemplateUsed(res, "fires/fire_year.html")
+        self.assertTemplateUsed(res, "fires/fire_type.html")
+        # test passed
